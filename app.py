a0a1653fbc3ff7c3f43cbc8af7017038653be2b0
Heya <fatemanusratheya@Fatemas-MacBook-Air.local>
2026-04-02 13:34:38 +0600

    added app.py code

--- app.py
+++ app.py
@@ -146,4 +146,5 @@ with gr.Blocks(title="Bangla Fake News Detector") as demo:
         """)
 
 if __name__ == "__main__":
-    demo.launch(share=False, server_name="127.0.0.1", server_port=7860)
+    demo.launch(share=False, server_name="127.0.0.1", server_port=7860) 
+    
